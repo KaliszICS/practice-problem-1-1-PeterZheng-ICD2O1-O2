3b3e7220841fee6d905c6856e9f14a6d4ce243bf
PeterZheng-ICD2O1-O2 <Peter.zheng@student.tdsb.on.ca>
2024-09-13 18:23:30 +0000

    l

--- PP1_1.py
+++ PP1_1.py
@@ -5,13 +5,17 @@ def q1():
 
 def q2():
   #Write Assignment code here
-  
+  print ("Hello World")
 def q3():
   #Write Assignment code here
-  
+  print('1')
+  print('2')
+  print('3')
+  print('4')
+  print('5')'
 def q4():
   #Write Assignment code here
-
+  print( "3 x 3 = 9")
 #Do not alter anything after this line
 q1()
 q2()
